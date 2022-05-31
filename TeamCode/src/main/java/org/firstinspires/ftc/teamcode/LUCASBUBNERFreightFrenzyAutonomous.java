15258549ccff0c69540fb2b4e10387fdac070432
Lucas Bubner <lkbubner@proton.me>
2022-05-31 17:49:16 +0930

    Cleaned up files a little

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LUCASBUBNERFreightFrenzyAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LUCASBUBNERFreightFrenzyAutonomous.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @SuppressWarnings("unused")
 @Autonomous(name = "LUCASBUBNERFreightFrenzyAutonomous")
+@Disabled // Code declared in work progress: 31/05/22
 public class LUCASBUBNERFreightFrenzyAutonomous extends LinearOpMode {
 
     @Override
