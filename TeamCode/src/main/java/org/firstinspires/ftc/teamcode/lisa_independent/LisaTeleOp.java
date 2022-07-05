51ca062ed581365ca24857879bdf0826c9fb9507
holo911 <lkbubner@proton.me>
2022-07-05 15:59:47 +0930

    Imported final classes from Blocks to Java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lisa_independent/LisaTeleOp.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.lisa_independent;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "<LISA-I> TeleOp")
+public class LisaTeleOp extends LinearOpMode {
+
+  @Override
+  public void runOpMode() {
+    DcMotor leftMotor = hardwareMap.get(DcMotor.class, "Left Motor");
+    DcMotor rightMotor = hardwareMap.get(DcMotor.class, "Right Motor");
+
+    double turn = 0;
+    double drive = 0;
+    double leftPower;
+    double rightPower;
+
+    waitForStart();
+    leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    if (opModeIsActive()) {
+      while (opModeIsActive()) {
+        turn = Range.clip(gamepad1.left_stick_x, -1.0, 1.0);
+        drive = gamepad1.right_trigger - gamepad1.left_trigger;
+        leftPower = Range.clip(drive + turn, -1.0, 1.0);
+        rightPower = Range.clip(drive - turn, -1.0, 1.0);
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+        telemetry.addData("Left Motor Power", leftMotor.getPower());
+        telemetry.addData("Right Motor Power", rightMotor.getPower());
+        telemetry.update();
+      }
+    }
+  }
+}
