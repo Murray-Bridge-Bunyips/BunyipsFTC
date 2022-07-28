a447718d16b60beb356923dd1dd9c2dd73738ba4
holo911 <lkbubner@proton.me>
2022-07-28 16:43:06 +0930

    Completed LisaTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lisa_independent/LisaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lisa_independent/LisaTeleOp.java
@@ -13,8 +13,8 @@ public class LisaTeleOp extends LinearOpMode {
     DcMotor leftMotor = hardwareMap.get(DcMotor.class, "Left Motor");
     DcMotor rightMotor = hardwareMap.get(DcMotor.class, "Right Motor");
 
-    double turn = 0;
-    double drive = 0;
+    double turn;
+    double drive;
     double leftPower;
     double rightPower;
 
@@ -24,19 +24,30 @@ public class LisaTeleOp extends LinearOpMode {
     rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-    if (opModeIsActive()) {
-      while (opModeIsActive()) {
-        // Motor efficiency is halved as they are very strong
-        turn = Range.clip(gamepad1.left_stick_x, -0.5, 0.5);
-        drive = gamepad1.right_trigger - gamepad1.left_trigger;
-        leftPower = Range.clip(drive + turn, -0.5, 0.5);
-        rightPower = Range.clip(drive - turn, -0.5, 0.5);
+    while (opModeIsActive()) {
+        // Motor efficiency is halved as they are very strong, unless A is pressed which is overdrive or B is pressed which is smoothdrive
+        if (gamepad1.a) {
+          turn = Range.clip(gamepad1.left_stick_x, -1, 1);
+          drive = gamepad1.right_trigger - gamepad1.left_trigger;
+          leftPower = Range.clip(drive + turn, -1, 1);
+          rightPower = Range.clip(drive - turn, -1, 1);
+        } else if (gamepad1.b) {
+            turn = Range.clip(gamepad1.left_stick_x, -0.25, 0.25);
+            drive = gamepad1.right_trigger - gamepad1.left_trigger;
+            leftPower = Range.clip(drive + turn, -0.25, 0.25);
+            rightPower = Range.clip(drive - turn, -0.25, 0.25);
+        } else {
+            turn = Range.clip(gamepad1.left_stick_x, -0.5, 0.5);
+            drive = gamepad1.right_trigger - gamepad1.left_trigger;
+            leftPower = Range.clip(drive + turn, -0.5, 0.5);
+            rightPower = Range.clip(drive - turn, -0.5, 0.5);
+        }
         leftMotor.setPower(leftPower);
         rightMotor.setPower(rightPower);
         telemetry.addData("Left Motor Power", leftMotor.getPower());
         telemetry.addData("Right Motor Power", rightMotor.getPower());
         telemetry.update();
-      }
     }
+
   }
 }
