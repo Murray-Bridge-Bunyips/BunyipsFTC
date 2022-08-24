e594b7754543ca782ef1c7c04424264fdf2d1e7b
Lucas Bubner <lkbubner@proton.me>
2022-08-24 12:45:15 +0930

    Added encoder task (needs class imports)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lisa/LisaEncoderDriveTask.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.lisa;
+
+public class LisaEncoderDriveTask extends BaseTask implements Task {
+
+    private final LisaDrive drive;
+    private final double leftDistance;
+    private final double rightDistance;
+    private final double leftPower;
+    private final double rightPower;
+
+    public LisaEncoderDriveTask(BunyipsOpMode opMode, double time, LisaDrive drive, double leftCM, double rightCM, double leftPower, double rightPower) {
+        super(opMode, telemetry);
+        this.drive = drive;
+
+        final double TICKS_PER_REVOLUTION = 0; // TODO
+        final double WHEEL_DIAMETER_CM = 0; // TODO
+
+        this.leftDistance = (leftCM * 10) * TICKS_PER_REVOLUTION / ((WHEEL_DIAMETER_CM / 10) * Math.PI);
+        this.rightDistance = (rightCM * 10) * TICKS_PER_REVOLUTION / ((WHEEL_DIAMETER_CM / 10) * Math.PI);
+        this.leftPower = leftPower;
+        this.rightPower = rightPower;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        drive.setEncoder(true);
+        drive.setTargetPosition(leftDistance, rightDistance);
+        drive.setPower(leftPower, rightPower);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished() || drive.targetPositionReached();
+    }
+
+    @Override
+    public void run() {
+        if (isFinished()) {
+            drive.setPower(0, 0);
+            drive.update();
+            drive.setEncoder(false);
+            return;
+        }
+        drive.update();
+    }
+}
