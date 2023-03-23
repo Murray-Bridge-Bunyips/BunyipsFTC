bc6ecda2c328c99a60cd0c81f9a7c51e08e1cb42
Lachlan Paul <lachlanpaul03@gmail.com>
2023-03-23 17:49:49 +1030

    Rough arm positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jerry/components/JerryArm.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jerry/components/JerryArm.kt
@@ -226,8 +226,8 @@ class JerryArm(
         4: Pole position 3
     */
     companion object {
-        // TODO: Update these values to match the actual arm positions
-        private val LIFT_POSITIONS = intArrayOf(20, 80, 350, 500, 700)
+        // These arm positions have been roughly calibrated, but are not fully accurate
+        private val LIFT_POSITIONS = intArrayOf(0, 20, 80, 130, 150, 175, 200, 230)
         // i am contributing nothing to the project -lachlan
         // i am contributing a little bit to the project -lachlan
         // i am contributing a lot to the project -lachlan
