b9f2456d435a3c20789ff074b46926318ffd8b6f
Lachlan Paul <lachlanpaul03@gmail.com>
2023-07-25 16:40:18 +0930

    Add required things for conf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinomighty/components/DinoConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dinomighty/components/DinoConfig.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode.dinomighty.components;
 
 public class DinoConfig {
+    //    front_servo = hardwareMap.get(Servo.class, "front_servo");
+    //    gripper = hardwareMap.get(Servo.class, "gripper");
+    //    right_front = hardwareMap.get(DcMotor.class, "right_front");
+    //    right_rear = hardwareMap.get(DcMotor.class, "right_rear");
+    //    arm = hardwareMap.get(DcMotor.class, "arm");
+    //    left_front = hardwareMap.get(DcMotor.class, "left_front");
+    //    left_rear = hardwareMap.get(DcMotor.class, "left_rear");
 }
