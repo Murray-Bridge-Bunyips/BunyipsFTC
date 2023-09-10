3c3652f6aab68891e46428ad56f7ad5c26f27828
Lachlan Paul <lachlanpaul03@gmail.com>
2023-09-10 18:34:59 +0930

    Began working on a universal autonomous. Will continue work next time as well as start working on other robot functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/AutonomousBunyipsOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/AutonomousBunyipsOpMode.java
@@ -1,8 +1,39 @@
 package org.firstinspires.ftc.teamcode.common;
 
+import org.firstinspires.ftc.teamcode.common.tasks.AutoTask;
+import org.firstinspires.ftc.teamcode.common.tasks.Task;
+
+import java.util.ArrayDeque;
+
 /**
  * OpMode abstraction for Autonomous operation using the BunyipsOpMode ecosystem.
  * @author Lucas Bubner, 2023
  * @author Lachlan Paul, 2023
  */
-// TODO
+
+abstract public class AutonomousBunyipsOpMode extends BunyipsOpMode {
+
+    private final ArrayDeque<AutoTask> tasks = new ArrayDeque<>();
+
+    /**
+     * Can be called to add custom tasks in a robot's autonomous
+     * @param newTask task to add to the run queue
+     */
+    public void addTask(AutoTask newTask) {
+        tasks.add(newTask);
+    }
+
+    @Override
+    protected void activeLoop(){
+        AutoTask currentTask = tasks.peekFirst();
+        if (currentTask == null) {
+            finish();
+            return;
+        }
+        currentTask.run();
+        if (currentTask.isFinished()) {
+            tasks.removeFirst();
+        }
+    }
+
+}
