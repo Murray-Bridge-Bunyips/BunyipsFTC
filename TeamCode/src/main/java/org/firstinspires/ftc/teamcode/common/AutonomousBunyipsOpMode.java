f64a9abbeef40da47832c9cfd4bdc0c7a2376151
Lucas Bubner <lkbubner@proton.me>
2023-09-10 05:15:12 +0000

    Split off Vision debugger, create ABYO

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/AutonomousBunyipsOpMode.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.common;
+
+/**
+ * OpMode abstraction for Autonomous operation using the BunyipsOpMode ecosystem.
+ * @author Lucas Bubner, 2023
+ * @author Lachlan Paul, 2023
+ */
+// TODO
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glados/debug/GLaDOSVisionDebug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glados/debug/GLaDOSVisionDebug.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.glados.teleop;
+package org.firstinspires.ftc.teamcode.glados.debug;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -8,22 +8,26 @@ import org.firstinspires.ftc.teamcode.common.Vision;
 import org.firstinspires.ftc.teamcode.glados.components.GLaDOSConfig;
 
 /**
- * TeleOp for GLaDOS robot FTC 15215
- * Functionality is currently unknown...
- *
+ * Vision debugger for AprilTag detections, GLaDOS bot.
  * @author Lucas Bubner, 2023
  */
-@TeleOp(name = "GLADOS: TeleOp", group = "GLADOS")
-public class GLaDOSTeleOp extends BunyipsOpMode {
+@TeleOp(name = "GLADOS: AprilTag Vision Debug", group = "GLADOS")
+public class GLaDOSVisionDebug extends BunyipsOpMode {
     private GLaDOSConfig config = new GLaDOSConfig();
+    private Vision vision;
 
     @Override
     protected void onInit() {
         config = (GLaDOSConfig) RobotConfig.newConfig(this, config, hardwareMap);
+        vision = new Vision(this, config.webcam);
+        // Start AprilTag vision processing
+        vision.init(Vision.Processors.APRILTAG);
+        vision.start(Vision.Processors.APRILTAG);
     }
 
     @Override
     protected void activeLoop() {
-        
+        vision.tick();
+        addTelemetry(vision.getAprilTagData().toString());
     }
 }
