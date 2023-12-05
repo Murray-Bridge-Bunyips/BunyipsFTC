febe0b4596d83584453894478bbb05dbb520d372
Lachlan Paul <lachlanpaul03@gmail.com>
2023-12-05 17:15:53 +1030

    Far Park, may be broken

--- TeamCode/Wheatley/src/main/java/org/murraybridgebunyips/wheatley/autonomous/WheatleyFarParkAuto.java
+++ TeamCode/Wheatley/src/main/java/org/murraybridgebunyips/wheatley/autonomous/WheatleyFarParkAuto.java
@@ -62,42 +62,47 @@ public class WheatleyFarParkAuto extends RoadRunnerAutonomousBunyipsOpMode<Mecan
 
         switch ((StartingPositions) selectedOpMode.getObj()) {
             case RED_LEFT:
-                addNewTrajectory()
-                        .setTimeout(-1)
-                        .forward(Inches.fromCM(175))
-                        .turn(Math.toRadians(-90))
-                        .forward(Inches.fromCM(340))
+                addNewTrajectory(new Pose2d(-36.05, -71.43, Math.toRadians(90.00)))
+                        .splineTo(new Vector2d(-36.99, -33.21), Math.toRadians(91.42))
+                        .splineTo(new Vector2d(62.16, -12.96), Math.toRadians(0.00))
                         .build();
                 break;
 
             case BLUE_LEFT:
-                addNewTrajectory(new Pose2d(-36.94, 70.42, Math.toRadians(270.00)))
-                        .setTimeout(-1)
-                        .splineTo(new Vector2d(-25.45, 35.16), Math.toRadians(6.83))
+//                addNewTrajectory(new Pose2d(-36.99, 71.05, Math.toRadians(270.00)))
+//                        .splineTo(new Vector2d(-30.00, 37.55), Math.toRadians(-7.71))
+//
+//                        // Since we could possibly interfere with our teammate's Autonomous,
+//                        // we wait a few seconds to give them time to move.
+//                        // TODO: Time code and change wait to make sure we can still make it
+//                        // .waitSeconds(10)
+//                        .splineTo(new Vector2d(32.07, 37.37), Math.toRadians(373.92))
+//                        .splineTo(new Vector2d(62.92, 60.65), Math.toRadians(360.00))
+//                        .build();
 
-                        // Since we could possibly interfere with our teammate's Autonomous,
-                        // we wait a few seconds to give them time to move.
-                        .waitSeconds(10)
-                        .splineTo(new Vector2d(25.45, 39.52), Math.toRadians(11.92))
-                        .splineTo(new Vector2d(61.50, 59.92), Math.toRadians(0.00))
+                addNewTrajectory(new Pose2d(-36.99, 71.05, Math.toRadians(270.00)))
+                        .splineTo(new Vector2d(-36.29, 37.67), Math.toRadians(-7.71))
+                        .splineTo(new Vector2d(32.07, 37.37), Math.toRadians(373.92))
+                        .splineTo(new Vector2d(50.81, 57.48), Math.toRadians(20.35))
+                        .splineTo(new Vector2d(79.85, 60.43), Math.toRadians(360.00))
                         .build();
+
+
+
                 break;
 
             case RED_RIGHT:
-                addNewTrajectory(new Pose2d(-36.49, -72.20, Math.toRadians(90.00)))
-                        .setTimeout(-1)
-                        .forward(Inches.fromCM(25))
-                        .strafeRight(Inches.fromCM(260))
+                addNewTrajectory(new Pose2d(-36.99, -71.05, Math.toRadians(90.00)))
+                        .splineTo(new Vector2d(-29.03, -37.55), Math.toRadians(-7.71))
+                        .splineTo(new Vector2d(32.07, -37.37), Math.toRadians(-13.92))
+                        .splineTo(new Vector2d(62.92, -60.65), Math.toRadians(0.00))
                         .build();
                 break;
 
             case BLUE_RIGHT:
-                // RED_LEFT and BLUE_RIGHT should pretty much be the same, just taking different turns
-                addNewTrajectory(new Pose2d(-35.31, 72.00, Math.toRadians(90.00)))
-                        .setTimeout(-1)
-                        .forward(Inches.fromCM(175))
-                        .turn(Math.toRadians(90))
-                        .forward(Inches.fromCM(340))
+                addNewTrajectory(new Pose2d(-36.05, 71.43, Math.toRadians(270.00)))
+                        .splineTo(new Vector2d(-36.99, 33.21), Math.toRadians(268.58))
+                        .splineTo(new Vector2d(61.40, 12.58), Math.toRadians(0.00))
                         .build();
                 break;
         }
