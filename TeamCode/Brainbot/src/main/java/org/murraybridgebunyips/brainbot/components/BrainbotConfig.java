5e5e04161083646a651682aaf2c8c1e4699c3c58
Lucas Bubner <lkbubner@proton.me>
2024-02-09 22:09:19 +1030

    Add brainbot (fake robot)

---
+++ TeamCode/Brainbot/src/main/java/org/murraybridgebunyips/brainbot/components/BrainbotConfig.java
@@ -0,0 +1,10 @@
+package org.murraybridgebunyips.brainbot.components;
+
+import org.murraybridgebunyips.bunyipslib.RobotConfig;
+
+public class BrainbotConfig extends RobotConfig {
+    @Override
+    protected void configureHardware() {
+
+    }
+}
