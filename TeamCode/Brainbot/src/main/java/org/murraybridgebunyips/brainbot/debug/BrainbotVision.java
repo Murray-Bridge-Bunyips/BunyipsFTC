839f157b598f6d8c6cf530b6417042b03c4060a4
Lucas Bubner <lkbubner@proton.me>
2024-02-10 16:30:26 +1030

    testing

--- TeamCode/Brainbot/src/main/java/org/murraybridgebunyips/brainbot/debug/BrainbotVision.java
+++ TeamCode/Brainbot/src/main/java/org/murraybridgebunyips/brainbot/debug/BrainbotVision.java
@@ -1,10 +1,15 @@
 package org.murraybridgebunyips.brainbot.debug;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.murraybridgebunyips.brainbot.components.BrainbotConfig;
 import org.murraybridgebunyips.bunyipslib.BunyipsOpMode;
+import org.murraybridgebunyips.bunyipslib.Dbg;
+import org.murraybridgebunyips.bunyipslib.cameras.C920;
 import org.murraybridgebunyips.bunyipslib.vision.Vision;
+import org.murraybridgebunyips.bunyipslib.vision.processors.AprilTag;
+import org.murraybridgebunyips.bunyipslib.vision.processors.TeamProp;
 import org.murraybridgebunyips.bunyipslib.vision.processors.WhitePixel;
 
 @TeleOp(name = "Vision Test (Brainbot)")
@@ -16,9 +21,16 @@ public class BrainbotVision extends BunyipsOpMode {
     protected void onInit() {
         config.init(this);
         vision = new Vision(this, config.camera);
-        WhitePixel whitePixel = new WhitePixel();
-        vision.init(whitePixel);
-        vision.start(whitePixel);
+        WhitePixel wp = new WhitePixel();
+        TeamProp tp = new TeamProp(255, 0, 0);
+        vision.init(wp, tp);
+        vision.start(wp, tp);
+        vision.startDashboardSender();
+    }
+
+    @Override
+    protected void onStart() {
+        vision.setDashboardProcessor("whitepixel");
     }
 
     @Override
