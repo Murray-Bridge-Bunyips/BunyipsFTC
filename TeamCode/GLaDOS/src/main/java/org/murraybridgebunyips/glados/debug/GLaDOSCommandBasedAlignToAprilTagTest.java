816914db7220efd3db0f30da4845fc9365619f80
Lucas Bubner <lkbubner@proton.me>
2024-03-01 14:04:37 +1030

    MoveToAprilTagTask progress

--- TeamCode/GLaDOS/src/main/java/org/murraybridgebunyips/glados/debug/GLaDOSCommandBasedAlignToAprilTagTest.java
+++ TeamCode/GLaDOS/src/main/java/org/murraybridgebunyips/glados/debug/GLaDOSCommandBasedAlignToAprilTagTest.java
@@ -7,16 +7,10 @@ import org.murraybridgebunyips.bunyipslib.CommandBasedBunyipsOpMode;
 import org.murraybridgebunyips.bunyipslib.Controller;
 import org.murraybridgebunyips.bunyipslib.drive.DualDeadwheelMecanumDrive;
 import org.murraybridgebunyips.bunyipslib.drive.MecanumDrive;
-import org.murraybridgebunyips.bunyipslib.pid.PIDController;
-import org.murraybridgebunyips.bunyipslib.tasks.AlignToAprilTagTask;
-import org.murraybridgebunyips.bunyipslib.tasks.AlignToPixelTask;
 import org.murraybridgebunyips.bunyipslib.tasks.HolonomicDriveTask;
 import org.murraybridgebunyips.bunyipslib.tasks.MoveToAprilTagTask;
-import org.murraybridgebunyips.bunyipslib.tasks.MoveToPixelTask;
 import org.murraybridgebunyips.bunyipslib.vision.Vision;
 import org.murraybridgebunyips.bunyipslib.vision.processors.AprilTag;
-import org.murraybridgebunyips.bunyipslib.vision.processors.MultiColourThreshold;
-import org.murraybridgebunyips.bunyipslib.vision.processors.centerstage.WhitePixel;
 import org.murraybridgebunyips.glados.components.GLaDOSConfigCore;
 
 /**
@@ -60,7 +54,7 @@ public class GLaDOSCommandBasedAlignToAprilTagTest extends CommandBasedBunyipsOp
     protected void assignCommands() {
         drive.setDefaultTask(new HolonomicDriveTask<>(gamepad1, drive, () -> false));
         scheduler().whenPressed(Controller.User.ONE, Controller.LEFT_BUMPER)
-                .run(new MoveToAprilTagTask<>(gamepad1, drive, aprilTag, new PIDController(0.36, 0.6, 0.0), new PIDController(0.36, 0.6, 0.0), new PIDController(0.67, 0.25, 0.0)))
+                .run(new MoveToAprilTagTask<>(gamepad1, drive, aprilTag, -1))
                 .finishingWhen(() -> !gamepad1.left_bumper);
     }
 }
