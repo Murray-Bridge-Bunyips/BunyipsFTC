9629fe0453e33acc2c9e99b829f2feaf3d5e58e6
Lucas Bubner <lkbubner@proton.me>
2024-04-02 18:25:46 +1030

    Controller work

---
+++ TeamCode/GLaDOS/src/main/java/org/murraybridgebunyips/glados/debug/GLaDOSDeadwheelDebug.java
@@ -0,0 +1,26 @@
+package org.murraybridgebunyips.glados.debug;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.murraybridgebunyips.bunyipslib.BunyipsOpMode;
+import org.murraybridgebunyips.glados.components.GLaDOSConfigCore;
+
+/**
+ * Debugging OpMode for reading the two deadwheel positions.
+ */
+@TeleOp(name="Deadwheel Debug")
+@Disabled
+public class GLaDOSDeadwheelDebug extends BunyipsOpMode {
+    private final GLaDOSConfigCore config = new GLaDOSConfigCore();
+
+    @Override
+    protected void onInit() {
+        config.init();
+    }
+
+    @Override
+    protected void activeLoop() {
+        addTelemetry("L:% R:%", config.parallelDeadwheel.getCurrentPosition(), config.perpendicularDeadwheel.getCurrentPosition());
+    }
+}
