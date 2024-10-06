b330433f4653e8a600f43ade954dc782bd45d9a6
Lucas Bubner <lkbubner@proton.me>
2024-10-06 18:50:18 +1030

    Cross conversion from ColourBlob to ContourData

--- TeamCode/Cellphone/src/main/java/org/murraybridgebunyips/cellphone/debug/CellphonePixelTest.java
+++ TeamCode/Cellphone/src/main/java/org/murraybridgebunyips/cellphone/debug/CellphonePixelTest.java
@@ -1,12 +1,16 @@
 package org.murraybridgebunyips.cellphone.debug;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ColorSpace;
 import org.firstinspires.ftc.vision.opencv.ImageRegion;
-import org.firstinspires.ftc.vision.opencv.PredominantColorProcessor;
 import org.murraybridgebunyips.bunyipslib.BunyipsOpMode;
 import org.murraybridgebunyips.bunyipslib.vision.Vision;
-import org.murraybridgebunyips.bunyipslib.vision.processors.ColourSensor;
+import org.murraybridgebunyips.bunyipslib.vision.processors.ColourLocator;
+import org.murraybridgebunyips.bunyipslib.vision.processors.centerstage.WhitePixel;
 import org.murraybridgebunyips.cellphone.components.CellphoneConfig;
 
 /**
@@ -35,16 +39,17 @@ public class CellphonePixelTest extends BunyipsOpMode {
 //        );
         visionB = new Vision(config.cameraB);
 //        MultiColourThreshold proc = new MultiColourThreshold(Pixels.createProcessors());
-//        ColourLocator proc = new ColourLocator(
-//                b -> b
-//                        .setRoi(ImageRegion.entireFrame())
-//                        .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
-//                        .setTargetColorRange(new ColorRange(ColorSpace.YCrCb, WhitePixel.LOWER_YCRCB, WhitePixel.UPPER_YCRCB))
-//        );
-        ColourSensor proc = new ColourSensor(ImageRegion.asUnityCenterCoordinates(-0.2, 0.2, 0.2, -0.2), PredominantColorProcessor.Swatch.RED);
+        ColourLocator proc = new ColourLocator(
+                b -> b
+                        .setRoi(ImageRegion.entireFrame())
+                        .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                        .setTargetColorRange(new ColorRange(ColorSpace.YCrCb, WhitePixel.LOWER_YCRCB, WhitePixel.UPPER_YCRCB))
+        );
+//        ColourSensor proc = new ColourSensor(ImageRegion.asUnityCenterCoordinates(-0.2, 0.2, 0.2, -0.2), PredominantColorProcessor.Swatch.RED);
         visionB.init(proc);
         visionB.start(proc);
         visionB.startPreview();
+//        Task testTask = new AlignToContourTask(Seconds.of(3), visionB, () -> proc.getData().stream().map(ColourBlob::toContourData).collect(Collectors.toList()), new PController(0));
     }
 
     @Override
