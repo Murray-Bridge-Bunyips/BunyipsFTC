f3133a08a5fd5ab5a9225079fa950dc3d1e592dc
Lucas Bubner <lkbubner@proton.me>
2024-10-10 12:35:16 +1030

    Servo test

--- TeamCode/Proto/src/main/java/org/murraybridgebunyips/proto/debug/ServoTest.java
+++ TeamCode/Proto/src/main/java/org/murraybridgebunyips/proto/debug/ServoTest.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/**
+ * test for servo
+ */
 @TeleOp
 @Photon
 public class ServoTest extends LinearOpMode {
@@ -13,6 +16,12 @@ public class ServoTest extends LinearOpMode {
         Servo servo = hardwareMap.get(Servo.class, "servo");
         servo.setPosition(1);
         waitForStart();
+        while (opModeIsActive()) {
+            servo.setPosition(0);
+            sleep(500);
+            servo.setPosition(1);
+            sleep(500);
+        }
     }
 }
 
