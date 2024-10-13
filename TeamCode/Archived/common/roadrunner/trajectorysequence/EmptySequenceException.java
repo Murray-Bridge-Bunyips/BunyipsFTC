2fc661be752f22899d211f7b7ce87044ab65868a
Lucas Bubner <lkbubner@proton.me>
2024-10-13 22:53:55 +1030

    Archive BunyipsLib code following RR v1 migration

---
+++ TeamCode/Archived/common/roadrunner/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,9 @@
+package org.murraybridgebunyips.bunyipslib.roadrunner.trajectorysequence;
+
+/**
+ * Thrown when a TrajectorySequence is empty.
+ *
+ * @since 1.0.0-pre
+ */
+public class EmptySequenceException extends RuntimeException {
+}
