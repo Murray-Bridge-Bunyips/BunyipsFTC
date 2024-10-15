b15dfcb2267ef78e8b4b3bd1545c4ff1efe9eb2f
Lachlan Paul <lachlanpaul03@gmail.com>
2024-10-15 16:39:51 +1030

    lights and other

--- TeamCode/Vance/src/main/java/org/murraybridgebunyips/vance/teleop/VanceTeleOp.java
+++ TeamCode/Vance/src/main/java/org/murraybridgebunyips/vance/teleop/VanceTeleOp.java
@@ -1,5 +1,9 @@
 package org.murraybridgebunyips.vance.teleop;
 
+import static org.murraybridgebunyips.bunyipslib.external.units.Units.Seconds;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.murraybridgebunyips.bunyipslib.CommandBasedBunyipsOpMode;
@@ -7,11 +11,14 @@ import org.murraybridgebunyips.bunyipslib.Controller;
 import org.murraybridgebunyips.bunyipslib.Controls;
 import org.murraybridgebunyips.bunyipslib.drive.MecanumDrive;
 import org.murraybridgebunyips.bunyipslib.drive.TriDeadwheelMecanumDrive;
+import org.murraybridgebunyips.bunyipslib.subsystems.BlinkinLights;
 import org.murraybridgebunyips.bunyipslib.subsystems.DualServos;
 import org.murraybridgebunyips.bunyipslib.subsystems.HoldableActuator;
 import org.murraybridgebunyips.bunyipslib.subsystems.Switch;
 import org.murraybridgebunyips.bunyipslib.tasks.HolonomicVectorDriveTask;
 import org.murraybridgebunyips.vance.Vance;
+
+import java.util.Objects;
 
 /**
  * TeleOp for Vance
@@ -23,8 +30,7 @@ import org.murraybridgebunyips.vance.Vance;
  * <b>gamepad2:</b><br>
  * Left Stick Y: Vertical Arm<br>
  * Right Stick Y: Horizontal Arm<br>
- * X: Open Both Claws<br>
- * B: Close Both Claws<br>
+ * X: Toggle Both Claws<br>
  * A: Toggle Claw Rotator<br>
  * Y: Toggle Basket Rotator<br>
  *
@@ -39,8 +45,7 @@ public class VanceTeleOp extends CommandBasedBunyipsOpMode {
     private Switch clawRotator;
     private Switch basketRotator;
     private DualServos claws;
-
-    //    private BlinkinLights lights;
+    private BlinkinLights lights;
 
     @Override
     protected void onInitialise() {
@@ -48,15 +53,15 @@ public class VanceTeleOp extends CommandBasedBunyipsOpMode {
         drive = new TriDeadwheelMecanumDrive(
                 robot.driveConstants, robot.mecanumCoefficients, robot.imu, robot.fl, robot.fr, robot.bl, robot.br,
                 robot.localiserCoefficients, robot.dwleft, robot.dwright, robot.dwx).withName("Drive");
-        verticalArm = new HoldableActuator(robot.verticalArm);
-        horizontalArm = new HoldableActuator(robot.horizontalArm);
+        verticalArm = new HoldableActuator(robot.verticalArm).withName("Vertical Arm");
+        horizontalArm = new HoldableActuator(robot.horizontalArm).withName("Horizontal Arm");
 
-        clawRotator = new Switch(robot.clawRotator);
-        basketRotator = new Switch(robot.basketRotator);
+        clawRotator = new Switch(robot.clawRotator).withName("Claw Rotator");
+        basketRotator = new Switch(robot.basketRotator).withName("Basket Rotator");
 
         // TODO: check open/close values
-        claws = new DualServos(robot.leftClaw, robot.rightClaw, 0.0, 1.0, 0.0, 1.0);
-//        lights = new BlinkinLights(config.lights, RevBlinkinLedDriver.BlinkinPattern.RAINBOW_FOREST_PALETTE);
+        claws = new DualServos(robot.leftClaw, robot.rightClaw, 1.0, 0.0, 0.0, 1.0);
+        lights = new BlinkinLights(robot.lights, RevBlinkinLedDriver.BlinkinPattern.LAWN_GREEN);
 
         gamepad1.set(Controls.AnalogGroup.STICKS, Controller.SQUARE);
     }
@@ -64,16 +69,14 @@ public class VanceTeleOp extends CommandBasedBunyipsOpMode {
     @Override
     protected void assignCommands() {
         operator().whenPressed(Controls.X)
-                        .run(claws.tasks.openBoth());
-        operator().whenPressed(Controls.B)
-                        .run(claws.tasks.closeBoth());
+                        .run(claws.tasks.toggleBoth());
         operator().whenPressed(Controls.A)
                         .run(clawRotator.tasks.toggle());
         operator().whenPressed(Controls.Y)
                         .run(basketRotator.tasks.toggle());
 
-        verticalArm.setDefaultTask(verticalArm.tasks.control(() -> gamepad2.lsy));
-        horizontalArm.setDefaultTask(horizontalArm.tasks.control(() -> gamepad2.rsy));
+        verticalArm.setDefaultTask(verticalArm.tasks.control(() -> -gamepad2.lsy));
+        horizontalArm.setDefaultTask(horizontalArm.tasks.control(() -> -gamepad2.rsy));
         drive.setDefaultTask(new HolonomicVectorDriveTask(gamepad1, drive, () -> false)
                     .withTranslationalPID(0.1, 0, 0)
                     .withRotationalPID(1, 0, 0.0001));
@@ -81,11 +84,19 @@ public class VanceTeleOp extends CommandBasedBunyipsOpMode {
 
     @Override
     protected void periodic() {
-//        // LED Management
-//        if (config.bottomLimit.isPressed()) { // Make an && for the top limit when it's reinstated
-//            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
-//        }
-//
+        Pose2d vel = Objects.requireNonNull(drive.getPoseVelocity());
+        boolean robotIsMoving = !vel.epsilonEquals(new Pose2d());
+
+        // LED Management
+        if (timer.elapsedTime().inUnit(Seconds) <= 150) {
+            // Endgame
+            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.STROBE_RED);
+        } else if (robotIsMoving) {
+            lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+        }
+
+        telemetry.add(robotIsMoving);
+
         // Some drivers have noted that they sometimes cannot tell whether a claw is open or closed.
         // Hopefully this helps. Update: It did :)
         // The actual string is set to the opposite of what you might expect, by driver request.
@@ -94,7 +105,7 @@ public class VanceTeleOp extends CommandBasedBunyipsOpMode {
         telemetry.add("Claw Rotator: " + (clawRotator.isOpen() ? "Closed" : "Open")).big();
         telemetry.add("Basket Rotator: " + (basketRotator.isOpen() ? "Closed" : "Open")).big();
         telemetry.add("---------\n");
-//
+
 //        telemetry.add("Top Switch Is %", config.topLimit.isPressed() ? "Pressed" : "Not Pressed");
 //        telemetry.add("Bottom Switch Is %", config.bottomLimit.isPressed() ? "Pressed" : "Not Pressed");
     }
