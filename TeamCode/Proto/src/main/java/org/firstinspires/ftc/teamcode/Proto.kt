c03e680b0f5e41171b2fd691deafa8e115f81877
Lucas Bubner <lkbubner@proton.me>
2025-03-17 17:05:33 +1030

    Resolving several bugs

--- TeamCode/Proto/src/main/java/org/firstinspires/ftc/teamcode/Proto.kt
+++ TeamCode/Proto/src/main/java/org/firstinspires/ftc/teamcode/Proto.kt
@@ -130,7 +130,6 @@ object Proto : RobotConfig() {
             it.setConstraints(TrapezoidProfile.Constraints(Constants.cr_v, Constants.cr_a))
             it.direction = Servo.Direction.REVERSE
             it.setPositionDeltaThreshold(0.02)
-            it.scaleRange(0.25, 1.0)
         }
 
         hw.clawLift = getHardware("cl", Motor::class.java) {
