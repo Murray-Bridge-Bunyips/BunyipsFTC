b6f7988614685eb84cb87f1301268e77fb2b66d7
Banjo <190065306+BanjoGoliath@users.noreply.github.com>
2025-05-06 16:53:21 +0930

    ok so i broke it??? maybe
    code sucks btw

--- TeamCode/Vance/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/Vance/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,13 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import java.lang.reflect.Field;
+
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsLib;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.CommandBasedBunyipsOpMode;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.executables.UserSelection;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.UnaryFunction;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.hardware.Controller;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.HolonomicVectorDriveTask;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.bases.Task;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Dbg;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Threads;
 
 @Config
@@ -19,6 +25,20 @@ public class TeleOp extends CommandBasedBunyipsOpMode {
 
     @Override
     protected void onInitialise() {
+        try {
+            // we are hacking. for some reason my controller is seen as a Gamepad instead of a Controller
+            // which causes some issues and crashes the code. so for now we just manually set it.
+            Field gp1 = OpMode.class.getSuperclass().getDeclaredField("gamepad1");
+            Field gp2 = OpMode.class.getSuperclass().getDeclaredField("gamepad2");
+            // overrides java safety(scary adn stoopid)
+            gp1.setAccessible(true);
+            gp2.setAccessible(true);
+            gp1.set(BunyipsLib.getOpMode(), gamepad1);
+            gp2.set(BunyipsLib.getOpMode(), gamepad2);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
         UserSelection<String> fieldCentricSelector = new UserSelection<>(
                 (s) -> FIELD_CENTRIC_ENABLED = s == null || s.equals("FIELD-CENTRIC"), "ROBOT-CENTRIC", "FIELD-CENTRIC")
                 .captionLayer(0, "SELECT DRIVE MODE");
@@ -37,6 +57,7 @@ public class TeleOp extends CommandBasedBunyipsOpMode {
         driver().whenPressed(Controls.A)
                 .run(hvdt::resetFieldCentricOrigin);
 
+        Dbg.log(BunyipsLib.getOpMode().gamepad1 instanceof Controller);
         operator().whenPressed(Controls.Y)
                 .run(vance.intake.tasks.run(Vance.EJECT).asPriority())
                 .finishIfButtonRetriggered();
