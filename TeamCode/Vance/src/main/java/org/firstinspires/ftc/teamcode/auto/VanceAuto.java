44b70397e3c4ba710044bec78cfdea82139809e3
Banjo <190065306+BanjoGoliath@users.noreply.github.com>
2025-05-20 16:51:24 +0930

    moar ortoe

--- TeamCode/Vance/src/main/java/org/firstinspires/ftc/teamcode/auto/VanceAuto.java
+++ TeamCode/Vance/src/main/java/org/firstinspires/ftc/teamcode/auto/VanceAuto.java
@@ -19,42 +19,30 @@ import dev.frozenmilk.util.cell.RefCell;
 @Autonomous
 public class VanceAuto extends AutonomousBunyipsOpMode {
     private final Vance vance = Vance.instance;
-    private final Pose2d basketPos = Geometry.poseFrom(new Vector2d(56.86, 55.04), Inches, 230.00, Degrees);
+    private final Pose2d basketPos = Geometry.poseFrom(new Vector2d(55.86, 53.04), Inches, 230.00, Degrees);
     private final int placeHeight = 500;
-
-    @Override
-    protected void onInitialise() {
-        setOpModes("Red", "Blue");
-    }
+    private final int pickUpPos = 156;
 
     @Override
     protected void onReady(@Nullable RefCell<?> selectedOpMode) {
-        if (selectedOpMode == null) {
-            return;
-        }
+        vance.drive.setPose(new Vector2d(36.03, 62.31), Inches, -90.00, Degrees);
+        vance.drive.makeTrajectory()
+                .splineTo(basketPos.position, basketPos.heading)
+                .addTask();
 
-        String res = (String) selectedOpMode.get(); // john drake approves!!!!!!!!!!!!
-
-        switch (res) {
-            case "Red":
-
-            case "Blue":
-                vance.drive.setPose(new Vector2d(36.03, 62.31), Inches, -90.00, Degrees);
-                vance.drive.makeTrajectory()
-                        .splineTo(basketPos.position, basketPos.heading)
-                        .addTask();
-
-                pickAndPlace(250);
-                pickAndPlace(20);
-                // TODO: TOOD: will need a unique one for third sample since it's against the wall
-        }
+        pickAndPlace(30);
+        pickAndPlace(20);
+        // TODO: TOOD: will need a unique one for third sample since it's against the wall
     }
 
 
     private void pickAndPlace(int degreeToTurnTo) {
-        vance.drive.makeTrajectory(basketPos).turn(degreeToTurnTo).addTask();
+        vance.drive.makeTrajectory(basketPos).turn(degreeToTurnTo, Degrees).addTask();
         add(vance.shoulder.tasks.home());
+        add(vance.elbow.tasks.goToProfiled(pickUpPos));
         add(vance.intake.tasks.runFor(Milliseconds.of(500), 1));
-        add(vance.shoulder.tasks.goTo(placeHeight));
+        add(vance.shoulder.tasks.goToProfiled(placeHeight));
+        vance.drive.makeTrajectory(basketPos).turn(degreeToTurnTo, Degrees).addTask();
+        add(vance.intake.tasks.runFor(Milliseconds.of(500), 1));
     }
 }
