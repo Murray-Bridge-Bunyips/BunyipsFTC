26028b2c3646e413ae6639c1cc88b8ae2f77796c
username-button <cdprofgo@gmail.com>
2025-08-26 16:55:16 +0930

    added (copied) field centric selection and started on spooky DoubleJointedArm.java

---
+++ TeamCode/Jonas/src/main/java/org/firstinspires/ftc/teamcode/components/DoubleJointedArm.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.components;
+
+
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsSubsystem;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.RobotConfig;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.HoldableActuator;
+
+public class DoubleJointedArm extends BunyipsSubsystem {
+//giulio was here and he is better
+
+    private HoldableActuator shoulder;
+    private HoldableActuator elbow;
+
+    public DoubleJointedArm(RobotConfig Jonas) {
+
+//        this.shoulder = (HoldableActuator) shoulder;
+
+    }
+
+    @Override
+    protected void periodic() {
+
+    }
+}
--- TeamCode/Jonas/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/Jonas/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -7,20 +7,25 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Jonas;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.CommandBasedBunyipsOpMode;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.executables.UserSelection;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Angle;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Measure;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.FieldOrientableDriveTask;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.HolonomicDriveTask;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.bases.Task;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.StartingConfiguration;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Dbg;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Storage;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Threads;
 
 @TeleOp(name = "TeleOp")
 public class MainTeleOp extends CommandBasedBunyipsOpMode {
     private final Jonas robot = new Jonas();
     public static StartingConfiguration.Position startingPos;
     private Measure<Angle> offset;
+
+    public static boolean FIELD_CENTRIC_ENABLED = true;
 
     @Override
     protected void onInitialise() {
@@ -39,11 +44,20 @@ public class MainTeleOp extends CommandBasedBunyipsOpMode {
         }
         Dbg.log(offset);
         startingPos = null;
+
+        UserSelection<String> fieldCentricSelector = new UserSelection<>(
+                (s) -> FIELD_CENTRIC_ENABLED = s == null || s.equals("FIELD-CENTRIC"), "ROBOT-CENTRIC", "FIELD-CENTRIC")
+                .captionLayer(0, "SELECT DRIVE MODE");
+        setInitTask(Task.task()
+                .init(() -> Threads.start("drive selector", fieldCentricSelector))
+                .isFinished(() -> !Threads.isRunning(fieldCentricSelector)));
     }
 
     @Override
     protected void assignCommands() {
-        FieldOrientableDriveTask driveTask = new HolonomicDriveTask(gamepad1, robot.drive).withFieldCentric(() -> true);
+        FieldOrientableDriveTask driveTask = new HolonomicDriveTask(gamepad1, robot.drive);
+        driveTask.withFieldCentric(() -> FIELD_CENTRIC_ENABLED).setAsDefaultTask();
+
         driveTask.setFieldCentricOffset(offset);
 
         driver().whenPressed(Controls.A)
