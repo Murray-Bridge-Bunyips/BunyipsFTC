46db069311d42ec9e364018e4cdfa6ed8a986ccc
Lucas Bubner <lkbubner@proton.me>
2025-11-11 20:13:45 +1030

    Use FtcDashboard's built-in method for serving images

--- TeamCode/Common/src/main/java/org/firstinspires/ftc/teamcode/common/FtcDashboardDecodeField.java
+++ TeamCode/Common/src/main/java/org/firstinspires/ftc/teamcode/common/FtcDashboardDecodeField.java
@@ -1,29 +1,18 @@
 package org.firstinspires.ftc.teamcode.common;
 
-import android.content.Context;
-
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.qualcomm.robotcore.util.WebHandlerManager;
-
-import org.firstinspires.ftc.ftccommon.external.WebHandlerRegistrar;
 
 import java.lang.reflect.Field;
 
-import fi.iki.elonen.NanoHTTPD;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.Hook;
 
 /**
  * Temporary shim to use the new field image on FTCDashboard.
  */
 public class FtcDashboardDecodeField {
-    @WebHandlerRegistrar
-    public static void injectDecodeField(Context context, WebHandlerManager manager) {
-        // serving the new field image on /field-image
-        manager.register("/field-image",
-                (s) -> NanoHTTPD.newChunkedResponse(
-                        NanoHTTPD.Response.Status.OK, "image/webp",
-                        context.getResources().openRawResource(R.raw.decode)
-                ));
+    @Hook(on = Hook.Target.PRE_INIT, priority = 7) // prior to all standard initialisation
+    public static void injectDecodeField() {
         try {
             Field defaultField = TelemetryPacket.class.getDeclaredField("DEFAULT_FIELD");
             defaultField.setAccessible(true);
@@ -32,7 +21,8 @@ public class FtcDashboardDecodeField {
                 DEFAULT_FIELD.clear(); // remove old operations and redo them using the new field we're serving
 
                 DEFAULT_FIELD.setAlpha(0.4);
-                DEFAULT_FIELD.drawImage("/field-image", 0, 0, 144, 144);
+                // image in Common module under src/main/assets/images
+                DEFAULT_FIELD.drawImage("/images/decode.webp", 0, 0, 144, 144);
                 DEFAULT_FIELD.setAlpha(1.0);
                 DEFAULT_FIELD.drawGrid(0, 0, 144, 144, 7, 7);
             }
