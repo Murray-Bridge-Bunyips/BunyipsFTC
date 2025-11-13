cccfaf3b2d978e5ee762c2b70747b02d10aafbe7
Lucas Bubner <lkbubner@proton.me>
2025-11-13 19:12:05 +1030

    Couple of code cleanliness patches

--- TeamCode/Damon/src/main/java/org/firstinspires/ftc/teamcode/DamonTeleOp.java
+++ TeamCode/Damon/src/main/java/org/firstinspires/ftc/teamcode/DamonTeleOp.java
@@ -43,9 +43,6 @@ public class DamonTeleOp extends BunyipsOpMode {
         //Check if startConfig in null
         //Check if its red or blue
         //Asign the goal varible
-
-
-
         StartingConfiguration.Position startConfig = Storage.memory().lastKnownStartingConfiguration;
         Vector2d goal;
         if(startConfig == null){
@@ -61,30 +58,21 @@ public class DamonTeleOp extends BunyipsOpMode {
         }
 
         new HolonomicDriveTask(gamepad1, robot.drive).setAsDefaultTask();
+        gamepad1.button(RIGHT_BUMPER)
+                .whileTrue(new AlignToPointDriveTask(() -> goal, gamepad1, robot.drive).withAlignmentOffset(Degrees.of(180)));
+
         gamepad1.button(X).or(gamepad2.button(X))
                 .whileTrue(robot.intake.tasks.run(1));
-
         gamepad1.button(A).or(gamepad2.button(A))
                 .whileTrue(robot.intake.tasks.run(-1));
 
-        gamepad1.button(Y)
+        gamepad1.button(Y).or(gamepad2.axisGreaterThan(LEFT_TRIGGER, 0.9))
                 .whileTrue(robot.shooter.tasks.run(1));
-        gamepad2.axisGreaterThan(LEFT_TRIGGER, 0.9)
-                        .whileTrue(robot.shooter.tasks.run(1));
-
-
         gamepad1.button(LEFT_BUMPER).or(gamepad2.button(LEFT_BUMPER))
                 .whileTrue(robot.shooter.tasks.run(0.85));
 
-
-        gamepad1.button(B).and(shooterPid::atSetpoint)
+        gamepad1.button(B).or(gamepad2.button(RIGHT_BUMPER)).and(shooterPid::atSetpoint)
                 .whileTrue(robot.transferWheel.tasks.run(1));
-        gamepad2.button(RIGHT_BUMPER).and(shooterPid::atSetpoint)
-                .whileTrue(robot.transferWheel.tasks.run(1));
-
-        gamepad1.button(RIGHT_BUMPER)
-
-                .whileTrue(new AlignToPointDriveTask(() -> goal, gamepad1, robot.drive).withAlignmentOffset(Degrees.of(180)));
     }
 
     @Override
--- TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
+++ TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
@@ -75,13 +75,13 @@ public class ForwardDumpAndCollect extends AutonomousBunyipsOpMode {
         robot.drive.makeTrajectory(last.get(), poseMap)
                 .splineTo(new Vector2d(-7.4, -27.0), -Math.PI / 2)
                 .setVelConstraints(Vel.ofMax(0.3, FieldTilesPerSecond))
-                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(16.5), 1).during(robot.transfer.tasks.run(-1)))
+                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(15), 1).during(robot.transfer.tasks.run(-1)))
                 .splineTo(new Vector2d(-7.4, -34.9), -Math.PI / 2) // first ball
                 .waitSeconds(3)
                 .splineTo(new Vector2d(-7.4, -40.7), -Math.PI / 2) // second ball
                 .waitSeconds(3)
                 .splineTo(new Vector2d(-7.4, -58.2), -Math.PI / 2) // third ball
-                .waitSeconds(5)
+                .waitSeconds(3.5)
                 .resetVelConstraints()
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-37.2, -27.5, Math.PI / 4), Math.toRadians(135))
