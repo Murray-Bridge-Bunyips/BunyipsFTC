892976aabc8853a22243b909f45037ff019744b2
zaghazada25 <zaghazada25@mbhs.sa.edu.au>
2025-11-13 17:01:08 +1030

    Auto refinements

--- TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
+++ TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
@@ -6,17 +6,22 @@ import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Fie
 import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Inches;
 import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Seconds;
 import static au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.StartingConfiguration.blueRight;
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.StartingConfiguration.redLeft;
 
 import androidx.annotation.Nullable;
 
+import com.acmerobotics.roadrunner.IdentityPoseMap;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseMap;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.AutonomousBunyipsOpMode;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.roadrunner.MirroredPoseMap;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.roadrunner.constraints.Vel;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.bases.Task;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.groups.ParallelTaskGroup;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.StartingConfiguration;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Ref;
 import dev.frozenmilk.util.cell.RefCell;
@@ -42,17 +47,20 @@ public class ForwardDumpAndCollect extends AutonomousBunyipsOpMode {
 
     @Override
     protected void onInitialise() {
+        setOpModes(
+                blueRight().tile(1.3).forward(FieldTiles.of(5).minus(Inches.of(12))).rotate(Degrees.of(143)),
+                redLeft().tile(1.3).forward(FieldTiles.of(5).minus(Inches.of(12))).rotate(Degrees.of(-143))
+        ).assignButton(0, 0, Controls.X).assignButton(0, 1, Controls.B);
         robot.init();
     }
 
     @Override
     protected void onReady(@Nullable RefCell<?> selectedOpMode) {
-        StartingConfiguration.Position start = blueRight()
-                .tile(1.3)
-                .forward(FieldTiles.of(5).minus(Inches.of(12)))
-                .rotate(Degrees.of(143))
-                .build();
+        if (selectedOpMode == null) return;
+        StartingConfiguration.Position start = (StartingConfiguration.Position) selectedOpMode.get();
+        PoseMap poseMap = start.isRed() ? new MirroredPoseMap() : new IdentityPoseMap();
         RefCell<Pose2d> last = Ref.empty();
+        // giulio was here
         luncheonInterval = new ParallelTaskGroup(
                 robot.outtake.tasks.run(1),
                 robot.transfer.tasks.run(1)
@@ -60,21 +68,26 @@ public class ForwardDumpAndCollect extends AutonomousBunyipsOpMode {
                         .after(Seconds.of(1))
         ).timeout(Seconds.of(5));
         robot.drive.setPose(start.toFieldPose());
-        robot.drive.makeTrajectory()
+        robot.drive.makeTrajectory(poseMap)
                 .strafeTo(new Vector2d(-37.2, -27.5))
                 .addTask(last);
         add(luncheonInterval);
-        robot.drive.makeTrajectory(last.get())
+        robot.drive.makeTrajectory(last.get(), poseMap)
                 .splineTo(new Vector2d(-7.4, -27.0), -Math.PI / 2)
                 .setVelConstraints(Vel.ofMax(0.3, FieldTilesPerSecond))
-                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(5), 1))
-                .splineTo(new Vector2d(-7.4, -58.2), -Math.PI / 2)
+                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(16.5), 1).during(robot.transfer.tasks.run(-1)))
+                .splineTo(new Vector2d(-7.4, -34.9), -Math.PI / 2) // first ball
+                .waitSeconds(3)
+                .splineTo(new Vector2d(-7.4, -40.7), -Math.PI / 2) // second ball
+                .waitSeconds(3)
+                .splineTo(new Vector2d(-7.4, -58.2), -Math.PI / 2) // third ball
+                .waitSeconds(5)
                 .resetVelConstraints()
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-37.2, -27.5, Math.PI / 4), Math.toRadians(135))
                 .addTask();
         add(luncheonInterval);
-        robot.drive.makeTrajectory(last.get())
+        robot.drive.makeTrajectory(last.get(), poseMap)
                 .strafeTo(new Vector2d(-56, -27))
                 .addTask();
     }
