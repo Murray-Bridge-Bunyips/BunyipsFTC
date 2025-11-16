60af29b8cda26d0c753b336e45df3622e876e802
Lucas Bubner <lkbubner@proton.me>
2025-11-16 16:49:47 +1030

    WE ARE GOING TO SYDNEY

--- TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
+++ TeamCode/Lilbro5000/src/main/java/org/firstinspires/ftc/teamcode/ForwardDumpAndCollect.java
@@ -75,13 +75,13 @@ public class ForwardDumpAndCollect extends AutonomousBunyipsOpMode {
         robot.drive.makeTrajectory(last.get(), poseMap)
                 .splineTo(new Vector2d(-7.4, -27.0), -Math.PI / 2)
                 .setVelConstraints(Vel.ofMax(0.3, FieldTilesPerSecond))
-                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(6), 1).during(robot.transfer.tasks.run(-1)))
+                .afterTime(0, robot.intake.tasks.runFor(Seconds.of(5), 1).during(robot.transfer.tasks.run(-1)))
                 .splineTo(new Vector2d(-7.4, -34.9), -Math.PI / 2) // first ball
-                .waitSeconds(1)
+                .waitSeconds(0)
                 .splineTo(new Vector2d(-7.4, -40.7), -Math.PI / 2) // second ball
-                .waitSeconds(1)
+                .waitSeconds(0)
                 .splineTo(new Vector2d(-7.4, -58.2), -Math.PI / 2) // third ball
-                .waitSeconds(1.5)
+                .waitSeconds(0.5)
                 .resetVelConstraints()
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-37.2, -27.5, Math.PI / 4), Math.toRadians(135))
