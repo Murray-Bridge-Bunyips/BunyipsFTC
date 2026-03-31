990dc2459a4e031317dc7271532df575a653522b
username-button <cdprofgo@gmail.com>
2026-03-31 16:47:31 +1030

    Table boilerplate

--- TeamCode/Table/src/main/java/org/firstinspires/ftc/teamcode/Table.java
+++ TeamCode/Table/src/main/java/org/firstinspires/ftc/teamcode/Table.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Table {
+    public static class Hardware {
+        //Placeholder
+    }
+
+    public final Hardware hw = new Hardware();
+
+//    @Override
+    protected void onRuntime() {
+        //Placeholder
+    }
 }
