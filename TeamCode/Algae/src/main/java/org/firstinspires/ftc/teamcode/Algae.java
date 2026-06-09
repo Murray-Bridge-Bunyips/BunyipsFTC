e8cc577736d421dadff671f02ed428b5f312e397
username-button <cdprofgo@gmail.com>
2026-06-09 16:59:00 +0930

    algae boilerplate

--- TeamCode/Algae/src/main/java/org/firstinspires/ftc/teamcode/Algae.java
+++ TeamCode/Algae/src/main/java/org/firstinspires/ftc/teamcode/Algae.java
@@ -1,4 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Algae {
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.RobotConfig;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.drive.SimpleMecanumDrive;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Algae extends RobotConfig {
+    public static class Hardware {
+        /**
+         * Control 0: configName
+         */
+        public DcMotor motor;
+
+
+        /**
+         * Expansion 0: configName
+         */
+        public DcMotor expMotor;
+
+
+        /**
+         * Expansion 0: configName
+         */
+        public Servo expServo;
+    }
+
+    /**
+     * 4-Wheels MecanumDrive
+     */
+    public SimpleMecanumDrive drive;
+
+    public final Hardware hw = new Hardware();
+
+    @Override
+    protected void onRuntime() {
+
+    }
 }
