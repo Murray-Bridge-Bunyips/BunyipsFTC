885636a661e30f6c404e8a69b43ba6ce859d9d22
username-button <cdprofgo@gmail.com>
2026-06-18 17:02:17 +0930

    dinkybot programming

--- TeamCode/DinkyBot/src/main/java/org/firstinspires/ftc/teamcode/DinkyBot.java
+++ TeamCode/DinkyBot/src/main/java/org/firstinspires/ftc/teamcode/DinkyBot.java
@@ -1,40 +1,99 @@
 package org.firstinspires.ftc.teamcode;
 
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Seconds;
+
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.RobotConfig;
-import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.drive.SimpleMecanumDrive;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.control.pid.PIDFController;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.hardware.Motor;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.hardware.ServoEx;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.Actuator;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.Switch;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.subsystems.drive.SimpleTankDrive;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.Collections;
 
 public class DinkyBot extends RobotConfig {
     public static class Hardware {
         /**
-         * Control 0: configName
+         * Control 1: leftDrive
          */
-        public DcMotor motor;
+        public DcMotor leftDrive;
+
+        /**
+         * Control 2: rightDrive
+         */
+        public DcMotor rightDrive;
+
+        /**
+         * Control 3: flywheel
+         */
+        public DcMotor flywheel;
 
 
         /**
-         * Expansion 0: configName
+         * Control 5: pusher
          */
-        public DcMotor expMotor;
-
-
-        /**
-         * Expansion 0: configName
-         */
-        public Servo expServo;
+        public Servo pusher;
     }
 
     /**
-     * 4-Wheels MecanumDrive
+     * 2-Wheels SimpleTankDrive
      */
-    public SimpleMecanumDrive drive;
+    public SimpleTankDrive drive;
+
+    /**
+     * Flywheel Actuator
+     */
+    public Actuator flywheel;
+
+    /**
+     * Pusher Switch
+     */
+    public Switch pusher;
 
     public final Hardware hw = new Hardware();
 
+    public double kP = 1;
+    public double kI = 0;
+    public double kD = 0;
+    public double kF = 0.87;
+
     @Override
     protected void onRuntime() {
+        hw.leftDrive = getHardware("leftDrive", DcMotorEx.class, (d) -> {
+            d.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            d.setDirection(DcMotor.Direction.REVERSE);
+        });
+        hw.rightDrive = getHardware("rightDrive", DcMotorEx.class, (d) -> {
+            d.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            d.setDirection(DcMotor.Direction.REVERSE);
+        });
 
+        hw.flywheel = getHardware("flywheel", Motor.class, (d) -> {
+            d.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            d.setDirection(DcMotor.Direction.REVERSE);
+
+            PIDFController pidf = new PIDFController(kP, kI, kD, kF);
+            d.setRunUsingEncoderController(1, 1800 /* theoretrically 2380 but it only approaches 1800 */, pidf);
+            d.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        });
+
+        hw.pusher = getHardware("pusher", ServoEx.class, (d) -> {
+            d.setEndToEndTime(Seconds.of(0.8));
+            d.scaleRange(0, 1);
+        });
+
+        drive = new SimpleTankDrive(Collections.singletonList(hw.leftDrive), Collections.singletonList(hw.rightDrive))
+                .withName("Drive");
+
+        flywheel = new Actuator(hw.flywheel)
+                .withName("Flywheel");
+
+        pusher = new Switch(hw.pusher)
+                .withName("Pusher");
     }
 }
--- TeamCode/DinkyBot/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpacificatorially.java
+++ TeamCode/DinkyBot/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpacificatorially.java
@@ -1,11 +1,25 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.transforms.Controls.A;
+
+import org.firstinspires.ftc.teamcode.DinkyBot;
+
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.BunyipsOpMode;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.HolonomicDriveTask;
 
 public class TeleOpacificatorially extends BunyipsOpMode {
+    private final DinkyBot robot = new DinkyBot();
+
     @Override
     protected void onInit() {
+        robot.init();
 
+        robot.drive.setDefaultTask(new HolonomicDriveTask(gamepad1, robot.drive));
+
+        robot.flywheel.setDefaultTask(robot.flywheel.tasks.control(() -> -gamepad2.right_stick_y));
+
+        gamepad2.button(A)
+                .onTrue(robot.pusher.tasks.toggle());
     }
 
     @Override
