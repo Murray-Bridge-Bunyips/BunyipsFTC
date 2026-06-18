45a25d461b4ea0d7b4d5ff04cad6c5bdbe7b6391
username-button <cdprofgo@gmail.com>
2026-06-18 16:21:24 +0930

    added DinkyBot since Algae wont be ready in time

---
+++ TeamCode/DinkyBot/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoOmittedMouse.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import androidx.annotation.Nullable;
+
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.AutonomousBunyipsOpMode;
+import dev.frozenmilk.util.cell.RefCell;
+
+public class AutoOmittedMouse extends AutonomousBunyipsOpMode {
+    @Override
+    protected void onInitialise() {
+
+    }
+
+    @Override
+    protected void onReady(@Nullable RefCell<?> selectedOpMode) {
+
+    }
+}
