7d14a598f202d2f9fed86c70bd0dfc172c7ffd11
username-button <cdprofgo@gmail.com>
2026-08-06 15:48:14 +0930

    minor spelling mistake

--- TeamCode/Jonas/src/main/java/org/firstinspires/ftc/teamcode/Jonas.java
+++ TeamCode/Jonas/src/main/java/org/firstinspires/ftc/teamcode/Jonas.java
@@ -108,7 +108,7 @@ public class Jonas extends RobotConfig {
             d.setDirection(DcMotor.Direction.REVERSE);
 
             PIDFController pidf = new PIDFController(kP, kI, kD, kF);
-            d.setRunUsingEncoderController(1, 1800 /* theoretrically 2380 but it only approaches 1800 */, pidf);
+            d.setRunUsingEncoderController(1, 1800 /* theoretically 2380 but it only approaches 1800 */, pidf);
             d.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         });
 
